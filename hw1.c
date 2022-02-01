b8ebbb283ba59257c50767d7bbcde0b584dd1bc1
PavanNalla <98801499+PavanNalla@users.noreply.github.com>
2022-02-01 01:17:52 -0600

    Update hw1.c

--- hw1.c
+++ hw1.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <errno.h>
 #define MAX_LEN 256
 struct count
 { 
     char keyword[10];
     int word_count;
 };
-//void keycount(char* input,struct count i)
+// If the input matches with any of the keywords, increment the corresponding keyword count by 1.
 void keycount(char* input,struct count *i,int a)
 {
     if(strcmp(input,i[a-1].keyword)==0)
@@ -17,6 +15,7 @@ void keycount(char* input,struct count *i,int a)
         i[a-1].word_count++;
     }
 }
+// To display the keyword table.
 void print_table(struct count* i,int n)
 {
     int a;
@@ -35,18 +34,17 @@ int main(int argc, char* keywords[])
     size_t input_len = 0;
     ssize_t char_num;
     struct count *i;
-
-    i=(struct count *)malloc(argc*sizeof(struct count));
-   
+    // Allocate the memomy to the structure based on number of keywords in the command line arguments
+    i=(struct count *)malloc(argc*sizeof(struct count)); 
+    // Copy the keywords in to the structure
     for(a=1; a<argc; a++)
     {
         i[a-1].word_count=0;
         strcpy(i[a-1].keyword,keywords[a]);
     }
 
-
     
-  /* read the user input using the getlin() until the NULL is found  */
+    // must take input using getline() method until NULL is found
     while ((char_num = getline (&input_string, &input_len, stdin)) > 0)
     {
         s=strtok(input_string," ");
